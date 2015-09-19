a92e6d3cef96e00e826d603c31f6b135ff34fcd1
Sushil Bhattarai <basuchara.sushil@gmail.com>
2015-09-19 15:52:44 +0545

    Disabled the message

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
+/*
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 console.log('Example app listening at http://%s:%s', host, port);
 });
+*/
